f16aeb479a5c5bd62861704b1984c902cdb6c22a
Tim Winfred <twinfred@gmail.com>
2021-05-03 20:56:36 -0700

    added react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,12 +1,29 @@
 import React from 'react';
+import { Switch, Route, Link } from 'react-router-dom';
+
 import './App.scss';
 
 import Homepage from './Pages/homepage';
 
+const CategoryPage = (props) => {
+  console.log(props);
+  let categoryId = props.match.params.categoryId;
+  return (
+    <div>
+      <Link to="/">Go Home</Link>
+      <button onClick={() => props.history.push('/shop/' + (parseInt(categoryId) + 1))}>Next</button>
+      <h1>Category Page {categoryId}</h1>
+    </div>
+  )
+};
+
 function App() {
   return (
     <>
-      <Homepage/>
+      <Switch>
+        <Route exact path="/" component={Homepage} />
+        <Route path="/shop/:categoryId" component={CategoryPage} />
+      </Switch>
     </>
   );
 }
